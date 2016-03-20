362aefb48eabbd17374cb2c0bf97a401ebc92bca
Dylan Piercey <pierceydylan@gmail.com>
2016-03-20 14:32:31 -0600

    Move styles to nprogress template to allow the progress bar to work in situations where the dom may have been removed during rendering.

--- README.md
+++ README.md
@@ -26,12 +26,13 @@ app.use(progress({ speed: 500, easing: "ease" }));
 	color:   "green", // The default color of the progress bar and spinner.
 	spinner: true,    // Show the spinning circle along with the bar.
 	parent:  "body",  // The element that will contain the progress bar.
+	style:   true     // Uses inline styles to create the progress bar. Set to false to use your own.
 }
 ```
 
 # Styling
 
-All styles are injected into the document when the page is loaded. You can the unminified styles [here](https://github.com/rstacruz/nprogress/blob/master/nprogress.css) and feel free to override them!
+All styles are injected into the document when the page is loaded. You can see the unminified styles [here](https://github.com/rstacruz/nprogress/blob/master/nprogress.css). To use your own styles use `style: false` to disable the inline styles.
 
 ### Contributions
 
--- client/index.js
+++ client/index.js
@@ -1,21 +1,19 @@
-var bar       = require("nprogress");
-var insertCSS = require("insert-css");
-var statuses  = require("statuses");
-var started   = false;
+var bar      = require("nprogress");
+var statuses = require("statuses");
+var started  = false;
 
 module.exports = function (opts) {
 	opts = opts || {};
+
+	var color = "color" in opts ? opts.color : "#000";
+	var css   = "#nprogress{pointer-events:none}#nprogress .bar{background:" + color + ";position:fixed;z-index:1031;top:0;left:0;width:100%;height:2px}#nprogress .peg{display:block;position:absolute;right:0;width:100px;height:100%;box-shadow:0 0 10px " + color + ",0 0 5px " + color + ";opacity:1;-webkit-transform:rotate(3deg) translate(0,-4px);-ms-transform:rotate(3deg) translate(0,-4px);transform:rotate(3deg) translate(0,-4px)}#nprogress .spinner{display:block;position:fixed;z-index:1031;top:15px;right:15px}#nprogress .spinner-icon{width:18px;height:18px;box-sizing:border-box;border:2px solid transparent;border-top-color:" + color + ";border-left-color:" + color + ";border-radius:50%;-webkit-animation:nprogress-spinner .4s linear infinite;animation:nprogress-spinner .4s linear infinite}.nprogress-custom-parent{overflow:hidden;position:relative}.nprogress-custom-parent #nprogress .bar,.nprogress-custom-parent #nprogress .spinner{position:absolute}@-webkit-keyframes nprogress-spinner{0%{-webkit-transform:rotate(0)}100%{-webkit-transform:rotate(360deg)}}@keyframes nprogress-spinner{0%{transform:rotate(0)}100%{transform:rotate(360deg)}}";
+	var style = opts.style === false ? "" : "<style>" + css + "</style>";
+
 	bar.configure({
 		parent: "parent" in opts ? opts.parent : undefined,
-		showSpinner: "spinner" in opts ? opts.spinner : true
+		showSpinner: "spinner" in opts ? opts.spinner : true,
+		template: style + bar.settings.template
 	});
-
-	// Inject css before all other styles to make overriding css easier.
-	var color = "color" in opts ? opts.color : "#94d60a";
-	insertCSS(
-		"#nprogress{pointer-events:none}#nprogress .bar{background:" + color + ";position:fixed;z-index:1031;top:0;left:0;width:100%;height:2px}#nprogress .peg{display:block;position:absolute;right:0;width:100px;height:100%;box-shadow:0 0 10px " + color + ",0 0 5px " + color + ";opacity:1;-webkit-transform:rotate(3deg) translate(0,-4px);-ms-transform:rotate(3deg) translate(0,-4px);transform:rotate(3deg) translate(0,-4px)}#nprogress .spinner{display:block;position:fixed;z-index:1031;top:15px;right:15px}#nprogress .spinner-icon{width:18px;height:18px;box-sizing:border-box;border:2px solid transparent;border-top-color:" + color + ";border-left-color:" + color + ";border-radius:50%;-webkit-animation:nprogress-spinner .4s linear infinite;animation:nprogress-spinner .4s linear infinite}.nprogress-custom-parent{overflow:hidden;position:relative}.nprogress-custom-parent #nprogress .bar,.nprogress-custom-parent #nprogress .spinner{position:absolute}@-webkit-keyframes nprogress-spinner{0%{-webkit-transform:rotate(0)}100%{-webkit-transform:rotate(360deg)}}@keyframes nprogress-spinner{0%{transform:rotate(0)}100%{transform:rotate(360deg)}}",
-		{ prepend: true }
-	);
 
 	return function progress (ctx, next) {
 		var req = ctx.req;
